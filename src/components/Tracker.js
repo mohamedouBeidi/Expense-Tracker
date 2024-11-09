8df662a97f3cdac4256fdf0081f2bf4467cf4115
unknown <beidimed73@gmail.com>
2024-11-09 20:49:55 +0100

    first Commit

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 import './tracker.css';
 const Tracker = () => {
     let [income, setIncome] = useState(0);
